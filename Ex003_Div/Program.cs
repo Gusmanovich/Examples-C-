cf4e853ee9a6e38c94dd2b6ac98da8d56e2f3a76
Almir <almir@example.com>
2022-05-10 21:38:03 +0300

    Особенности деления вещественных и целых чисел

--- Ex003_Div/Program.cs
+++ Ex003_Div/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = 12;
+int numberB = 5;
+int result = numberA / numberB; 
+Console.Write("Целочисленное деление    ");
+Console.WriteLine(result);
+
+double numberA1 = 12;
+double numberB1 = 5;
+double result1 = numberA1 / numberB1; 
+Console.Write("Вещественное деление    ");
+Console.WriteLine(result1);
