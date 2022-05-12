6028d20375abb87bdb6abe3e3f02fc04a26fb286
Almir <almir@example.com>
2022-05-12 21:16:39 +0300

    Особое приветствие

---
+++ Ex004_If_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ввведите ваше имя:   ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.Write("Здравствуй, МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
