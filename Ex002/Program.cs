887eb86a3b91a140dbead0d49d6eafcd22b40990
Almir <almir@example.com>
2022-05-12 20:59:15 +0300

    add random

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,4 +1,4 @@
-﻿int numberA = 3;
+﻿int numberA = new Random().Next(1,10); // Случайные числа из диапозона 1 2 3 ... 9
 int numberB = 15;
 int result = numberA + numberB;
 Console.WriteLine(result);
