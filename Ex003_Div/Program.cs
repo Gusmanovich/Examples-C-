b9377a81db494374769aca2f1e11b33819b7ad88
Almir <almir@example.com>
2022-05-12 20:47:43 +0300

    явное числовое преобразование double в int

--- Ex003_Div/Program.cs
+++ Ex003_Div/Program.cs
@@ -1,11 +1,11 @@
-﻿int numberA = 12;
-int numberB = 5;
-int result = numberA / numberB; 
-Console.Write("Целочисленное деление    ");
+﻿double numberA = 12;
+double numberB = 5;
+double result = numberA / numberB; 
+Console.Write("Вещественное деление    ");
 Console.WriteLine(result);
 
-double numberA1 = 12;
-double numberB1 = 5;
-double result1 = numberA1 / numberB1; 
-Console.Write("Вещественное деление    ");
+int result1 = (int)numberA / (int)numberB;  // явное числовое преобразование double в int
+
+Console.Write("Целочисленное деление    ");
 Console.WriteLine(result1);
+
